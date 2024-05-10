b29ba76e998d7fd4032675ce7e3b5333b96e81af
daimyo1 <163930521+daimyo1@users.noreply.github.com>
2024-05-09 20:36:05 -0700

    2nd draft of ui for project

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-function openModal() {
-    
-	// Add is-active class on the modal
-	document.getElementById("modal1").classList.add("is-active");
-  }
-   
-  // Function to close the modal
-  function closeModal() {
-	document.getElementById("modal1").classList.remove("is-active");
-  }
-   
-  // Add event listeners to close the modal
-  // whenever user click outside modal
-  document.querySelectorAll(
-   ".modal-background, 
-	.modal-close,
-	.modal-card-head
-	.delete, 
-	.modal-card-foot
-	.button"
-  ).forEach(($el) => {
-			const $modal = $el.closest(".modal");
-			$el.addEventListener("click", () => {
-			 
-			// Remove the is-active class from the modal
-			$modal.classList.remove("is-active");
-		  });
-		});
-		 
-		// Adding keyboard event listeners to close the modal
-		document.addEventListener("keydown", (event) => {
-		const e = event || window.event;
-			if (e.keyCode === 27) { 
-			 
-			 // Using escape key 
-			  closeModal();
-			}
-		 });
+document.addEventListener('DOMContentLoaded', () => {
+	// Functions to open and close a modal
+	function openModal($el) {
+	  $el.classList.add('is-active');
+	}
+  
+	function closeModal($el) {
+	  $el.classList.remove('is-active');
+	}
+  
+	function closeAllModals() {
+	  (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+		closeModal($modal);
+	  });
+	}
+  
+	// Add a click event on buttons to open a specific modal
+	(document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+	  const modal = $trigger.dataset.target;
+	  const $target = document.getElementById(modal);
+  
+	  $trigger.addEventListener('click', () => {
+		openModal($target);
+	  });
+	});
+  
+	// Add a click event on various child elements to close the parent modal
+	(document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+	  const $target = $close.closest('.modal');
+  
+	  $close.addEventListener('click', () => {
+		closeModal($target);
+	  });
+	});
+  
+	// Add a keyboard event to close all modals
+	document.addEventListener('keydown', (event) => {
+	  if(event.key === "Escape") {
+		closeAllModals();
+	  }
+	});
+  });
